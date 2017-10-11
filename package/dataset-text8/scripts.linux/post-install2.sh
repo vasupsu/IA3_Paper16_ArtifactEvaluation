543a9a8ae33833ae3f6b5a78c9276783b54175e8
Grigori Fursin <Grigori.Fursin@cTuning.org>
2017-10-12 00:30:47 +0200

    * added datasets text8 & billion as CK soft and CK package * added above packages as run_time deps to program * added vars to customize program execution

---
+++ package/dataset-text8/scripts.linux/post-install2.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#
+# Installation script for Caffe.
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, 2017
+#
+
+# PACKAGE_DIR
+# INSTALL_DIR
+
+cd ${INSTALL_DIR}
+gzip -d text8.gz -f
+
+return 0
